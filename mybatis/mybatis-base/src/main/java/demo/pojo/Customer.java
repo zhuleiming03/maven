ca40fe4bca89827d94de70f0612558e3fefb1272
zhuleiming03@gmail.com <zhuleiming03@gmail.com>
2023-06-30 11:14:35 +0800

    reset

--- mybatis/mybatis-base/src/main/java/demo/pojo/Customer.java
+++ mybatis/mybatis-base/src/main/java/demo/pojo/Customer.java
@@ -1,4 +1,4 @@
-package pojo;
+package demo.pojo;
 
 import java.math.BigDecimal;
 
--- mysql/mysql-statement/src/test/java/TransactionTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class TransactionTest {
-
-    @Test
-    public void main() {
-        new Account().tranfer("张三", "李四", 100f);
-    }
-}
